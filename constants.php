e8e62bc245e3ba91d0f6b8613c3f3646af3d7c66
shahlin <shahlin44@gmail.com>
2019-05-28 13:32:19 +0400

    Changed top period from month to day and added few constants

--- constants.php
+++ constants.php
@@ -6,6 +6,19 @@ define("SMARTY_PATH", 'lib/smarty-3.1.33/libs');
 // Templates directory path
 define("TEMPLATES_PATH", 'templates');
 
+// Subreddit to fetch data from
+define("SUBREDDIT", 'freefolk');
+
+// Top posts past day/month/year/all
+define("TOP_PERIOD", 'day');
+
+// Number of posts to fetch in a request
+// Max limit is 100
+// To fetch more than 100, use the after query parameter with the name of the last
+// post. This will get 100 posts after the last one. However you will have to 
+// run the request twice.
+define("POSTS_LIMIT", 100);
+
 // Reddit URL to get data from
-define("DATA_URL", 'https://www.reddit.com/r/freefolk/top.json?t=month&limit=100')
+define("DATA_URL", 'https://www.reddit.com/r/'.SUBREDDIT.'/top.json?t='.TOP_PERIOD.'&limit='.POSTS_LIMIT);
 ?>
